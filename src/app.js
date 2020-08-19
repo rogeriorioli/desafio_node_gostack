131f5a070e3147c5b8a6670b5dfa0e3343f99059
Rogerio Orioli <rogerio@designhope.com.br>
2020-08-18 23:00:29 -0300

    desafio node done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,74 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+  
+  app.get("/repositories", (request, response) => {
 
-app.post("/repositories", (request, response) => {
-  // TODO
+      return response.json(repositories)
+    // TODO
+  });
+  
+  app.post("/repositories", (request, response) => {
+    const  {  title , url , techs  } = request.body
+     
+    const repositorie = {
+      id : uuid(), 
+      title , 
+      url , 
+      techs,
+      likes : 0
+    }
+
+    repositories.push(repositorie)
+
+    return response.status(200).json(repositorie)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+      const {id} = request.params
+
+      const { title, url, techs } = request.body
+
+      const repositorieIndex  =  repositories.findIndex(repositorie =>  repositorie.id === id );
+
+      if(repositorieIndex < 0) { 
+        return response.status(400).json({message : 'not found '}) 
+      };
+
+      const repositorie = {
+        id,
+        title,
+        url,
+        techs,
+      }
+      repositories[repositorieIndex] = repositorie
+
+      return response.json(repositorie)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repositorieIndex  =  repositories.findIndex(repositorie =>  repositorie.id === id );
+
+  if(repositorieIndex < 0) { 
+    return response.status(400).json({message : 'not found '}) 
+  };
+
+   repositories.splice(repositorieIndex , 1)
+
+   return response.send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorie = repositories.find((rep) => rep.id === id);
+  repositorie.likes += 1;
+
+  return response.json(repositorie);
 });
 
 module.exports = app;
