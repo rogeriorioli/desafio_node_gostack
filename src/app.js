733a044ed01fa386e4ac482134ad459131b17a4e
Rogerio Orioli <rogerio@designhope.com.br>
2020-08-19 00:16:39 -0300

    test são zuados

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = new Array;
 
   
   app.get("/repositories", (request, response) => {
@@ -18,16 +18,14 @@ const repositories = [];
   });
   
   app.post("/repositories", (request, response) => {
-    const  {  title , url , techs  } = request.body
-     
+    const  { id = uuid(), title , url , techs, likes} = request.body
     const repositorie = {
-      id : uuid(), 
+      id,
+      likes : Number(0),
       title , 
-      url , 
+      url ,
       techs,
-      likes : 0
     }
-
     repositories.push(repositorie)
 
     return response.status(200).json(repositorie)
@@ -38,7 +36,7 @@ app.put("/repositories/:id", (request, response) => {
 
       const {id} = request.params
 
-      const { title, url, techs } = request.body
+      const { title, url, techs, likes } = request.body
 
       const repositorieIndex  =  repositories.findIndex(repositorie =>  repositorie.id === id );
 
@@ -51,6 +49,9 @@ app.put("/repositories/:id", (request, response) => {
         title,
         url,
         techs,
+      }
+      if(likes ==! repositories.likes) {
+        response.status(400).json({err : `not permited`})
       }
       repositories[repositorieIndex] = repositorie
 
@@ -68,16 +69,19 @@ app.delete("/repositories/:id", (request, response) => {
 
    repositories.splice(repositorieIndex , 1)
 
-   return response.send();
+   return response.status(204).json({message : 'deleted susseful'});
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
   const repositorie = repositories.find((rep) => rep.id === id);
+  if(!repositorie) {
+    response.status(400).json({err : 'not permited '})
+  }
   repositorie.likes += 1;
 
-  return response.json(repositorie);
+  return response.json(repositorie.likes);
 });
 
 module.exports = app;
